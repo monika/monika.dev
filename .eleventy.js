60bafbeb97cd0ff7fffe135ddd2e9687494dc9ed
Monika <monikahoex@gmail.com>
2019-12-23 19:49:21 -0800

    Remove unnecessary favicons

--- .eleventy.js
+++ .eleventy.js
@@ -24,10 +24,6 @@ module.exports = function(eleventyConfig) {
   // Pass these assets through
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
-  eleventyConfig.addPassthroughCopy('src/android-chrome-192x192.png');
-  eleventyConfig.addPassthroughCopy('src/android-chrome-512x512.png');
-  eleventyConfig.addPassthroughCopy('src/apple-touch-icon.png');
-  eleventyConfig.addPassthroughCopy('src/favicon-16x16.png');
   eleventyConfig.addPassthroughCopy('src/favicon-32x32.png');
   eleventyConfig.addPassthroughCopy('src/site.webmanifest');
 
