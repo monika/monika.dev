0269e9183e523d0f17f8e0598d613f7108cbc82d
Monika <monikahoex@gmail.com>
2020-01-05 18:36:56 -0800

    Add sitemap.xml, robots.txt, webmanifest

--- .eleventy.js
+++ .eleventy.js
@@ -23,9 +23,9 @@ module.exports = function(eleventyConfig) {
 
   // Pass these assets through
   eleventyConfig.addPassthroughCopy('src/images');
-  eleventyConfig.addPassthroughCopy('src/js');
+  eleventyConfig.addPassthroughCopy('src/robots.txt');
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
-  eleventyConfig.addPassthroughCopy('src/favicon-32x32.png');
+  eleventyConfig.addPassthroughCopy('src/favicon.png');
   eleventyConfig.addPassthroughCopy('src/site.webmanifest');
 
   // Add CSS to template formats
--- src/colophon.md
+++ src/colophon.md
@@ -1,5 +1,6 @@
 ---
 layout: default.njk
+sitemapIgnore: true
 ---
 
 ## Colophon
