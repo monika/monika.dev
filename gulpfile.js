ed028f5ca230508610b5e8adb517b9a417a18296
Monika <monikahoex@gmail.com>
2020-01-10 20:12:52 -0800

    Specifying images to resize

--- gulpfile.js
+++ gulpfile.js
@@ -22,90 +22,93 @@ function compileCSS() {
 // remaking templates if the /image directory updates this often */
 function resizeImages() {
   return gulp
-  .src('src/images/*.{jpg,png}')
-  .pipe(
-    responsive({
-      'img-*.{jpg,png}': [
-        // Define jpg at two resolutions
+    .src('src/images/img-*.{jpg,png}')
+    .pipe(
+      responsive(
         {
-          width: 200,
-          rename: {
-            suffix: '-small',
-            extname: '.jpg'
-          }
+          'img-*.{jpg,png}': [
+            // Define jpg at two resolutions
+            {
+              width: 200,
+              rename: {
+                suffix: '-small',
+                extname: '.jpg'
+              }
+            },
+            {
+              width: 200 * 2,
+              rename: {
+                suffix: '-small@2x',
+                extname: '.jpg'
+              }
+            },
+            {
+              width: 400,
+              rename: {
+                suffix: '-medium',
+                extname: '.jpg'
+              }
+            },
+            {
+              width: 400 * 2,
+              rename: {
+                suffix: '-medium@2x',
+                extname: '.jpg'
+              }
+            },
+            {
+              width: 800,
+              rename: {
+                suffix: '-large',
+                extname: '.jpg'
+              }
+            },
+            // Define webp at two resolutions
+            {
+              width: 200,
+              rename: {
+                suffix: '-small',
+                extname: '.webp'
+              }
+            },
+            {
+              width: 200 * 2,
+              rename: {
+                suffix: '-small@2x',
+                extname: '.webp'
+              }
+            },
+            {
+              width: 400,
+              rename: {
+                suffix: '-medium',
+                extname: '.webp'
+              }
+            },
+            {
+              width: 400 * 2,
+              rename: {
+                suffix: '-medium@2x',
+                extname: '.webp'
+              }
+            },
+            {
+              width: 800,
+              rename: {
+                suffix: '-large',
+                extname: '.webp'
+              }
+            }
+          ]
         },
         {
-          width: 200 * 2,
-          rename: {
-            suffix: '-small@2x',
-            extname: '.jpg'
-          }
-        },
-        {
-          width: 400,
-          rename: {
-            suffix: '-medium',
-            extname: '.jpg'
-          }
-        },
-        {
-          width: 400 * 2,
-          rename: {
-            suffix: '-medium@2x',
-            extname: '.jpg'
-          }
-        },
-        {
-          width: 800,
-          rename: {
-            suffix: '-large',
-            extname: '.jpg'
-          }
-        },
-        // Define webp at two resolutions
-        {
-          width: 200,
-          rename: {
-            suffix: '-small',
-            extname: '.webp'
-          }
-        },
-        {
-          width: 200 * 2,
-          rename: {
-            suffix: '-small@2x',
-            extname: '.webp'
-          }
-        },
-        {
-          width: 400,
-          rename: {
-            suffix: '-medium',
-            extname: '.webp'
-          }
-        },
-        {
-          width: 400 * 2,
-          rename: {
-            suffix: '-medium@2x',
-            extname: '.webp'
-          }
-        },
-        {
-          width: 800,
-          rename: {
-            suffix: '-large',
-            extname: '.webp'
-          }
+          // Global configuration for all images
+          quality: 90,
+          withMetadata: false
         }
-      ]
-    },
-    {
-      // Global configuration for all images
-      quality: 90,
-      withMetadata: false
-    })
-  ).pipe(gulp.dest('src/images-resized'));
+      )
+    )
+    .pipe(gulp.dest('src/images-resized'));
 }
 
 // Watch files
